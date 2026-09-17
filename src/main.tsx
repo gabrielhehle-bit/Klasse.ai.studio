b4798164caf139ecc6704658f932e48eba09e711
gabrielhehle-bit <gabrielhehle@gmail.com>
2026-09-17 15:55:57 +0200

    UI: große Einheitenplanung aktivieren

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
+import './planning-dialog.css';
 
 // Render app
 createRoot(document.getElementById('root')!).render(
