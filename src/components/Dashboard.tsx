6cfbfffc0e3978e0b009e71200269db16572e5dc
gabrielhehle-bit <gabrielhehle@gmail.com>
2026-09-19 17:53:27 +0200

    feat: place compact expandable class documentation in Heute dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,3 +1,4 @@
+import KlassenUeberblick from './KlassenUeberblick';
 import React, { useState, useEffect, useCallback } from "react";
 import { createPortal } from "react-dom";
 import { useApp } from "../context/AppContext";
@@ -4739,6 +4740,8 @@ Pädagogische Reflexionsnotiz: ${luuiseComment}`;
         weekEvents={weekEventsList}
         monthEvents={monthEventsList}
       />
+
+      <KlassenUeberblick />
 
       {/* Smart Status Dashboard */}
       {!simpleDashboardMode && (
